e968434435db1ab9e4ae7c801dcb40e731901097
chackco <chackco@gmail.com>
2019-08-06 11:17:58 +0700

    Create encode-decode.ps1

---
+++ encode-decode.ps1
@@ -0,0 +1,2 @@
+[System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("SecretMessage"))
+[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("U2VjcmV0TWVzc2FnZQ=="))
